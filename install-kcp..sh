ad98239d7b745a5e9b49c546c77b4d8337b482d5
FIREU666 <ochf86@163.com>
2017-04-06 14:20:07 +0800

    Update and rename kcpinstall.sh to install-kcp..sh

--- install-kcp..sh
+++ install-kcp..sh
@@ -35,7 +35,7 @@ shell_update(){
         fi
     fi
 }
-shell_download_link="https://raw.githubusercontent.com/FIREU666/onekey-kcp/master/kcpinstall.sh"
+shell_download_link="https://raw.githubusercontent.com/FIREU666/onekey-kcp/master/install-kcp.sh"
 program_version_link="https://raw.githubusercontent.com/FIREU666/onekey-kcp/master/version.sh"
 ss_libev_config="/etc/shadowsocks-libev/config.json"
 ssr_config="/usr/local/shadowsocksR/shadowsocksR.json"
